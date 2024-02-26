3771dc49c48ab0cd940883d2cf5cbd2b3cc1e4fe
omurbekavalbekov <omurbekavalbekov9@gmail.com>
2024-02-26 10:35:27 +0800

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int money = 80;
+
+        if (money > 500){
+            System.out.println("Pizza");
+        } else if (money < 500 && money >= 300){
+            System.out.println("Shavurma");
+        } else if (money >= 100 && money < 300){
+            System.out.println("Gamburger");
+        }else {
+            System.out.println("Doshirak");
+        }
     }
 }
